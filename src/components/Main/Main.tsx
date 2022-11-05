bab91af542aa01c38208576d208a816dbd8d30d6
Adran Carnavale <adran.carnavale@gmail.com>
2022-11-04 23:08:47 -0300

    feat: applies lazyLoading

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <>
+      <h1>Main component</h1>
+    </>
+  );
+};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,34 @@
-import { Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Main = lazy(() =>
+  import("../components/Main/Main").then((module) => ({ default: module.Main }))
+);
+
+const Login = lazy(() =>
+  import("../components/Login/Login").then((module) => ({
+    default: module.Login,
+  }))
+);
+
+const Profile = lazy(() =>
+  import("../components/Profile/Profile").then((module) => ({
+    default: module.Profile,
+  }))
+);
 
 export const AppRoutes = () => {
-  return <h1>Aqui ficarão as rotas</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Suspense>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
